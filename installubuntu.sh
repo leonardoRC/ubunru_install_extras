7f07cebc6339242f5fe5de3be5fc45ac20ddec63
leo <leonardo@octadigital.com.br>
2018-01-29 19:20:41 -0200

    adicionado pacotes do python necessarios

--- installubuntu.sh
+++ installubuntu.sh
@@ -6,7 +6,7 @@ echo deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) con
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
 
 apt-get update
-apt-get install vim ssh wget cifs-utils synaptic -y
+apt-get install vim ssh wget cifs-utils  -y
 apt-get install oracle-java8-installer -y
 apt-get install unity-tweak-tool compizconfig-settings-manager -y
 apt-get install p7zip p7zip-full p7zip-rar lzma lzma-dev -y  ##instalar 7zip com suporte a rar
@@ -14,6 +14,9 @@ apt-get install flashplugin-installer flashplugin-nonfree-extrasound -y ## insta
 apt-get install ubuntu-restricted-extras -y  ##Ubuntu-Restricted-Extras, Skype e Google Chrome
 ###### compiladores e afins
 apt-get install -y build-essential git git-svn
+apt-get install python-setuptools python-dev build-essential -y
+easy_install pip
+pip install virtualenv
 
 ###### gadget monitoramento
 apt-get install conky-all conky curl lm-sensors -y
